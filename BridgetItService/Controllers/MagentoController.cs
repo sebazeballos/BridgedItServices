e6b875380d103efa6284ad84c193e04be18e6ac3
Priyam1604 <priyampatel704@gmail.com>
2023-07-03 09:25:45 +1200

    Magento Controller

---
+++ BridgetItService/Controllers/MagentoController.cs
@@ -0,0 +1,28 @@
+﻿using BridgetItService.Contracts;
+using BridgetItService.Models;
+using Microsoft.AspNetCore.Mvc;
+using ShopifySharp;
+
+namespace BridgetItService.Controllers
+{
+    [Route("api/magento")]
+    [ApiController]
+    public class MagentoController : ControllerBase
+    {
+        private readonly IMagentoService _magentoService;
+        public MagentoController(IMagentoService magentoService)
+        {
+            _magentoService = magentoService;
+        }
+        //[HttpPost("/products")]
+        //public async Task<IActionResult> PostProducts(InfinityPosProducts products)
+        //{
+        //    return Ok(await _shopifyService.PublishProducts(products.Products);
+        //}
+
+        private IActionResult Ok(object v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
